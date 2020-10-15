34285b896015cf3ecf54b72e29011618daa38f72
Mrityunjay Kumar <53057286+mrityunjay-1@users.noreply.github.com>
2020-10-15 11:05:35 +0530

    Updates

---
+++ precache-manifest.c0ea90b91778c5f0987955adb349dc6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525f57725fecef9a68f583fe757d259e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6196285563bfb76ff19",
+    "url": "/static/css/main.11815f3c.chunk.css"
+  },
+  {
+    "revision": "457194b7e0ecfd70b79a",
+    "url": "/static/js/2.821bbcc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.821bbcc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6196285563bfb76ff19",
+    "url": "/static/js/main.25087150.chunk.js"
+  },
+  {
+    "revision": "7f42403ec663b31222d7",
+    "url": "/static/js/runtime-main.d596cfc0.js"
+  }
+]);
