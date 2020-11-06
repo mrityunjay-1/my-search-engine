7553474cebaeb5fe738cd9f42ab9e3c8369ef645
Mrityunjay Kumar <kumarmrityunjay7034@gmail.com>
2020-11-06 18:40:15 +0530

    Updates

---
+++ precache-manifest.dde015aa8a0c85b4c8f8cfd57346fc87.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a0f1006648b25a27b4cf1c4efce876",
+    "url": "/my-search-engine/index.html"
+  },
+  {
+    "revision": "5662e981f5d5d5811c19",
+    "url": "/my-search-engine/static/css/main.84ca6f9e.chunk.css"
+  },
+  {
+    "revision": "024772ce4f667f8b01d0",
+    "url": "/my-search-engine/static/js/2.49f70a1b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-search-engine/static/js/2.49f70a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5662e981f5d5d5811c19",
+    "url": "/my-search-engine/static/js/main.73872bfb.chunk.js"
+  },
+  {
+    "revision": "53bbe9dd23778ca034d0",
+    "url": "/my-search-engine/static/js/runtime-main.3d78dc48.js"
+  },
+  {
+    "revision": "2410bd35f85287ac216c0d1f93f1ae96",
+    "url": "/my-search-engine/static/media/logo.2410bd35.png"
+  },
+  {
+    "revision": "5a843f124a31d38957e60617067db086",
+    "url": "/my-search-engine/static/media/logo2.5a843f12.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c0ea90b91778c5f0987955adb349dc6a.js"
+  "/my-search-engine/precache-manifest.dde015aa8a0c85b4c8f8cfd57346fc87.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-search-engine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
